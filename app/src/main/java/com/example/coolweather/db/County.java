a87a7e86937da8ea21160800ec045923aa33e2a3
sunhuansheng <251630606@qq.com>
2018-04-24 09:20:54 +0800

    加入创建数据库和表的各项配置

--- app/src/main/java/com/example/coolweather/db/County.java
+++ app/src/main/java/com/example/coolweather/db/County.java
@@ -2,7 +2,7 @@ package com.example.coolweather.db;
 
 import org.litepal.crud.DataSupport;
 
-public class Country extends DataSupport {
+public class County extends DataSupport {
     private  int id;
     private  String countryName;
     private  String weatherId;
